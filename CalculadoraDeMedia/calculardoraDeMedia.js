190d576e2fdcf4125f508fb69c41554ca137f2d8
Carolaine Oliveira <122393936+CarolaineOliver@users.noreply.github.com>
2023-11-15 09:24:30 -0300

    Add files via upload

---
+++ CalculadoraDeMedia/calculardoraDeMedia.js
@@ -0,0 +1,7 @@
+let nomeAluno = prompt("Digite o nome do aluno:")
+let nota1 = parseFloat(prompt("Digite a primeira nota do aluno: (entre 0 e 10)"))
+let nota2 = parseFloat(prompt("Digite a segunda nota do aluno:"))
+let nota3 = parseFloat(prompt("Digite a terceira nota do aluno:"))
+let media = (nota1 + nota2 + nota3 )/ 3 
+
+console.log(media)

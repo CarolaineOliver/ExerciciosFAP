dcb104b8ea96dbf73d32623ce7b110c266622b05
Carolaine Oliveira <122393936+CarolaineOliver@users.noreply.github.com>
2023-11-30 22:43:39 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,13 @@
+function saudacaoSimples() {
+    console.log("Olá, vamos começar nossos calculos")
+    
+}
+
+function soma(primeiroValor, segundoValor) {
+    return primeiroValor + segundoValor;
+}
+
+const multiplicacao = (primeiroValor, segundoValor) => primeiroValor * segundoValor;
+
+
+console.log(saudacaoSimples(), soma(5,2), multiplicacao(4,7))
